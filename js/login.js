426c68b3013f8b91375e4e5fd61f61583964c57d
jiahao <15029218541@163.com>
2017-02-13 20:46:23 +0800

    create new file  01 2017-2-13

--- js/login.js
+++ js/login.js
@@ -1,7 +1,65 @@
-$('#mobile').focus(function () {
-    $('.msg-wrap').show();
+$(function () {
+    var oPhone=$('#mobile')[0];
+    var oPhoneTips=$('#phoneTips')[0];
+    oPhone.onblur=function () {
+        oPhoneRe=/^1\d{10}$/;
+        if(oPhoneRe.test(oPhone.value)){
+            oPhoneTips.innerHTML="<font color='green'>√</font>";
+            return true;
+        } else {
+            oPhoneTips.innerHTML="<font color='red'>X</font>";
+        }
+    }
+    
+    
+    //加载验证码
+    var checkArr=["1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var str='';
+    function checkCode() {
+        for(var i=0;i<4;i++){
+            var index=parseInt(Math.random()*checkArr.length);
+            str+=checkArr[index];
+        }
+    }
+    $('#get-code').click(function () {
+        $(this).html(str);
+         str='';
+        checkCode();
+        $(this).html(str).css('color','white');
+
+    });
+    $('#code').blur(function () {
+        if ($('#code').val()==str){
+            $('#get-code-msg').html("<font color='green'>√"+"</font>");
+        }else {
+            $('#get-code-msg').html("<font color='red'>X"+"</font>");
+        }
+    })
+//    设置密码
+    function pas(){
+        var rpass=/^([0-9]|[A-Z]|[a-z]){6,12}$/;
+        var pass1=$("#password").val();
+        var stu=rpass.test(pass1);
+        if(true){
+            $("#testPass1")[0].innerHTML="<font color='green'>√</font>";
+        }else{
+            $("#testPass1")[0].innerHTML="<font color='red'>X</font>";
+        }
+    }
+    //密码验证
+    $('#password').blur(function () {
+        pas();
+    });
+    $("#repassword")[0].onblur=function(){
+        var pass2=$("#repassword").val();
+        var pass1=$("#password").val();
+        if(pass2==pass1){
+            $("#testPass2")[0].innerHTML="<font color='green'>√</font>";
+        }else{
+            $("#testPass2")[0].innerHTML="<font color='red'>X</font>";
+        }
+    }
+
+
+
 })
-/*
-$('#code').blur(function () {
-    $('')
-})*/
