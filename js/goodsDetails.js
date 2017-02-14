86d3ba3b92016a4cb904fa922e8e60942af5af96
jiahao <15029218541@163.com>
2017-02-14 21:33:01 +0800

    create new file  02 2017-2-14

--- js/goodsDetails.js
+++ js/goodsDetails.js
@@ -1,3 +1,36 @@
 /**
  * Created by Administrator on 2017/2/14 0014.
  */
+$(function () {
+    $('.goods_li').hover(function () {
+        $(".divStart").eq($(this).index()).css("borderColor","#423e3e");
+    },
+    function () {
+        $(".divStart").eq($(this).index()).css("borderColor","#ffffff");
+    }
+    );
+
+    //鼠标移入显示二级菜单
+
+    $('#popnav').mouseenter(function () {
+        $('.nav_details').show();
+        $('.sn_nav').show();
+    });
+    $('#popnav').mouseleave(function () {
+        $('.nav_details').hide();
+        $('.sn_nav').hide();
+    });
+
+
+//    放大镜的制作
+    $('#spec_left').click(function () {
+        var oLeft=$('.J_carousel_list').width();
+        $('.J_carousel_list').animate({left:oLeft},500);
+        $('.J_carousel_list').css({left:0});
+
+    })
+
+
+
+
+})
